59060bf7d2c347cfc9b7dda00d510e7c3141f516
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-10 13:35:15 +0300

    completes challenge

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(user) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {user.bio && <p>{user.bio}</p>}
+
+     <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
